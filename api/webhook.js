310efec034d7a30d1bc03a943afd09f2a86221d3
wwpateon <altunaygulnaz@gmail.com>
2025-04-27 22:44:32 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -1,8 +1,4 @@
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log("Gelen Webhook:", req.body);
-    res.status(200).json({ message: 'Webhook alındı!' });
-  } else {
-    res.status(405).json({ message: 'Sadece POST isteği kabul ediliyor.' });
-  }
-}
+// Patronum, yeni kod bu sekilde olacak. Sonra "Commit Changes" ye basman lazim!
+
+export default async function handler(req, res) { if (req.method === 'POST') { console.log("Gelen Webhook:", req.body); res.status(200).json({ message: 'Webhook alındı!' }); } else { res.status(405).json({ message: 'Sadece POST isteği kabul ediliyor.' }); } }
+
